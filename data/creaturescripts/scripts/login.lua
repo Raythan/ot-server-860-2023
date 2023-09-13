1b497eec7f9636dc46b35782fb32c71dbf8a12c5
Raythan Padovani Abreu Machado <raythan100@yahoo.com.br>
2023-09-12 22:32:52 -0300

    ajuste

--- data/creaturescripts/scripts/login.lua
+++ data/creaturescripts/scripts/login.lua
@@ -1,7 +1,16 @@
 local config = {
 	loginMessage = getConfigValue('loginMessage'),
-	useFragHandler = getBooleanFromString(getConfigValue('useFragHandler'))
+	useFragHandler = getBooleanFromString(getConfigValue('useFragHandler')),
+	townId = getConfigValue('newPlayerTownId'),
+	pos = {x = getConfigValue('newPlayerSpawnPosX'), y = getConfigValue('newPlayerSpawnPosY'), z = getConfigValue('newPlayerSpawnPosZ')}
 }
+--posX = getConfigValue('newPlayerSpawnPosX')
+--posY = getConfigValue('newPlayerSpawnPosY')
+--posZ = getConfigValue('newPlayerSpawnPosZ')
+
+dofile("config.lua")
+
+local storageIdFreeOutfit = { 10022, 10023, 10024, 10025 }
 
 function onLogin(cid)
 	local loss = getConfigValue('deathLostPercent')
@@ -24,6 +33,8 @@ function onLogin(cid)
 			doPlayerSendTextMessage(cid, MESSAGE_STATUS_DEFAULT, str)
 			str = "Your last visit was on " .. os.date("%a %b %d %X %Y", lastLogin) .. "."
 		else
+			doPlayerSetTown(cid, config.townId)
+			doTeleportThing(cid, config.pos)
 			str = str .. " Please choose your outfit."
 			doPlayerSendOutfitWindow(cid)
 		end
@@ -36,20 +47,52 @@ function onLogin(cid)
 	else
 		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_ORANGE, "Hello, type 'account' to create an account or type 'recover' to recover an account.")
 	end
+	
+	if(getPlayerLevel(cid) < 31) then
+		doPlayerAddBlessing(cid, 1)
+		doPlayerAddBlessing(cid, 2)
+		doPlayerAddBlessing(cid, 3)
+		doPlayerAddBlessing(cid, 4)
+		doPlayerAddBlessing(cid, 5)
+		doSendMagicEffect(getPlayerPosition(cid), 28)
+		doPlayerSendTextMessage(cid,MESSAGE_EVENT_ADVANCE, 'You was blessed by the gods!')
+	end
 
 	if(not isPlayerGhost(cid)) then
 		doSendMagicEffect(getCreaturePosition(cid), CONST_ME_TELEPORT)
 	end
-
+	
+	-- registerCreatureEvent(cid, "advance")
+	registerCreatureEvent(cid, "AdvanceSave")
+	-- registerCreatureEvent(cid, "attackguild")
+	-- registerCreatureEvent(cid, "bluelegs")
+	-- registerCreatureEvent(cid, "demonOakDeath")
+	-- registerCreatureEvent(cid, "demonOakLogout")
+	--registerCreatureEvent(cid, "ExpVip")
+	--registerCreatureEvent(cid, "FimVip")
+	-- registerCreatureEvent(cid, "FirstItems")
 	registerCreatureEvent(cid, "Mail")
 	registerCreatureEvent(cid, "GuildMotd")
-
 	registerCreatureEvent(cid, "Idle")
+	-- registerCreatureEvent(cid, "KillingInTheNameOf")
+	-- registerCreatureEvent(cid, "levelplayer")
+	-- registerCreatureEvent(cid, "Mail")
+	-- registerCreatureEvent(cid, "PlayerAdvance")
+	-- registerCreatureEvent(cid, "PlayerKill")
+	--registerCreatureEvent(cid, "Promot")
+	-- registerCreatureEvent(cid, "PythiusDead")
+	registerCreatureEvent(cid, "ReportBug")
+	-- registerCreatureEvent(cid, "PlayerDeath")
 	if(config.useFragHandler) then
 		registerCreatureEvent(cid, "SkullCheck")
 	end
-
-	registerCreatureEvent(cid, "ReportBug")
-	registerCreatureEvent(cid, "AdvanceSave")
+	--registerCreatureEvent(cid, "VipReceive")
+	registerCreatureEvent(cid, "KillTask")
+	registerCreatureEvent(cid, "LookTask")
+	registerCreatureEvent(cid, "LogoutSave")
+	registerCreatureEvent(cid, "UpLevel")
+	
+	--restoreStorageWithTimeout(cid)
+	
 	return true
 end
