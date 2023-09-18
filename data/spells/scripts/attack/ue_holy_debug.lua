595a4e3b28357707d7f52ca0eb5cf6122404082e
Raythan Padovani Abreu Machado <raythan100@yahoo.com.br>
2023-09-18 09:21:11 -0300

    lixo teste

---
+++ data/spells/scripts/attack/ue_holy_debug.lua
@@ -0,0 +1,11 @@
+local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_HOLYDAMAGE)
+setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_HOLYAREA)
+setAttackFormula(combat, COMBAT_FORMULA_LEVELMAGIC, 1, 10000, 1, 10000)
+
+local area = createCombatArea(AREA_CROSS5X5)
+setCombatArea(combat, area)
+
+function onCastSpell(cid, var)
+	return doCombat(cid, combat, var)
+end
